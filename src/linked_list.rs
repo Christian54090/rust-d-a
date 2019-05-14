455cba78ca7fd54d338e4192a88f39ae97b6e2fc
Christian Sanders <christian.sanders@live.com>
2019-05-13 19:39:03 -0700

    Linked list pseudo code

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -8,3 +8,19 @@ struct Node<T> {
     elem: T,
     next: Link<T>,
 }
+
+impl<T> List<T> {
+    fn new() -> List<T> {
+        List { head: None }
+    }
+
+    fn push(&mut self, elem: T) {
+        // create new Box<Node>, set next to old head
+        // set head to new node
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        // set head to old head's next
+        // return old head
+    }
+}
