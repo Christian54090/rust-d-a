5a5a5ac85ae9629a7767b4bb181545dd947baed1
Christian Sanders <christian.sanders@live.com>
2019-05-14 16:35:38 -0700

    Moving variables, will try Rc

--- src/binary_search_tree.rs
+++ src/binary_search_tree.rs
@@ -17,29 +17,25 @@ impl Tree {
         }
     }
 
-    pub fn insert(self, data: i32) -> Tree {
+    pub fn insert(self, data: i32) {
         let new_node = Box::new(Node {
             data,
             left: None,
             right: None,
         });
 
-        if data > self.head.data {
-            let mut node = self.head;
+        let mut node = self.head;
+        if data > node.data {
             while node.right.is_some() {
                 node = node.right.unwrap();
             }
-
             node.right = Some(new_node);
         } else {
-            let mut node = self.head;
             while node.left.is_some() {
                 node = node.left.unwrap();
             }
-
             node.left = Some(new_node);
         }
-        Tree { head: self.head }
     }
 }
 
@@ -49,16 +45,16 @@ mod test {
 
     #[test]
     fn basics() {
-        let mut tree = Tree::new(4);
+        let tree = Tree::new(4);
         let head = tree.head;
 
         // inserting left
-        tree = tree.insert(2);
+        tree.insert(2);
         assert_eq!(head.data, 4);
         assert_eq!(head.left.unwrap().data, 2);
 
         // inserting right
-        tree = tree.insert(5);
+        tree.insert(5);
         assert_eq!(head.data, 4);
         assert_eq!(head.right.unwrap().data, 5);
     }
